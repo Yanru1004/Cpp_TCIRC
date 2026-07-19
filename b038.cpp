0693a98ae367ea2b6cd950fdda240da7b7e62b16
Yanru1004 <devil1004@livemail.tw>
2026-07-19 18:03:35 +0800

    20260719 b038_AC

--- b038.cpp
+++ b038.cpp
@@ -8,7 +8,8 @@ using namespace std;
 //主程式
 int main(){
     
-    int day,king_day=0,banana=0,k_banana=0;
+    int day,banana=0,k_banana=0;
+    size_t king_day = 0;
     vector<int> tax = {1,1};
 
     cin >> day; //取得天數
@@ -20,24 +21,30 @@ int main(){
         if (tax.size() == king_day+1){  //建立及確認稅收表
             tax.push_back(tax[king_day-1] + tax[king_day]);
         }
-        cout <<"Day-"<< king_day+1  << " Tax: "<< tax[king_day]<< '\n';
+        //cout <<"Day-"<< t << " King day: " << king_day+1  << " Tax: "<< tax[king_day]<< '\n';
         //革命判斷
         if (tax[king_day] < banana){
             //收稅
             banana -= tax[king_day];
             k_banana += (tax[king_day]*20);
-            cout << k_banana << '\n';
+            //cout << "King have " << k_banana << '\n';
+            //cout << "Monkey have " << banana << '\n'; 
             //香蕉腐敗
-            k_banana -= (k_banana/2);           
+            k_banana -= (k_banana/2);
+            //cout << "banana bad  last " << k_banana << '\n';
+             king_day += 1;
         }
         else{
             //發動革命
-            cout << "Revolution" << '\n';
-            break;
-        } 
+            int total_banana = (k_banana) + (banana *20);
+            //cout << "Revolution" << '\n';
+            //cout << "Total banana: " << total_banana << '\n';
+            banana = (total_banana * 0.95)/20;
+            k_banana = total_banana - (banana *20);
+            //cout << "monkey get: " << banana << "  monkey king get:  " << k_banana << '\n'; 
+            king_day = 0;
+        }        
     
-    cout << "bad "<<k_banana << '\n';
-     king_day += 1;
     }//for 迴圈尾端
-    cout << k_banana;
+    cout << banana;
 }
